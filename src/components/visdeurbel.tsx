b79b26f275c37dadc7f926ae6c384d18071c299e
snoot-booper <12149655+snoot-booper@users.noreply.github.com>
2025-04-05 12:32:37 +0200

    Update visdeurbel.tsx

--- src/components/visdeurbel.tsx
+++ src/components/visdeurbel.tsx
@@ -4,7 +4,7 @@ export function VisDeurbel() {
       <h2 className="text-3xl absolute top-4 left-4">🐟</h2>
       <iframe
         className="w-full h-full"
-        src="https://www.youtube-nocookie.com/embed/JOEdSqXEZnk?si=Rub5Mruyk66pZjSx&controls=0&autoplay=1&mute=1"
+        src="https://www.youtube-nocookie.com/embed/jSW9HrPLNBo?si=KvEJDUZktzI7Y8c_&controls=0&autoplay=1&mute=1"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
